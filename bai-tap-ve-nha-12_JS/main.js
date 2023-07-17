1173a0c7159d2105991af09791a788b736fb1d5d
KhanhNgo <62138525+khanh7516@users.noreply.github.com>
2023-07-18 04:04:28 +0700

    Summary

--- bai-tap-ve-nha-12_JS/main.js
+++ bai-tap-ve-nha-12_JS/main.js
@@ -1,89 +1,82 @@
 let todoList = [];
 let addBtn = document.querySelector(".add");
 let input = document.querySelector(".input");
-let editButtons = document.querySelectorAll(".edit-button");
-let id = 0;
 let list = document.querySelector(".list");
 
+todoList.push({ id: 0, title: "Đi chơi", status: false });
+todoList.push({ id: 1, title: "Học bài", status: true });
+todoList.push({ id: 2, title: "Đá bóng", status: false });
 
-const info = document.createElement('p');
-info.innerHTML = 'Danh sách công việc trống!';
-document.body.appendChild(info);
-
-const checkEmptyList = () => {
-    if(todoList.length === 0) info.style.display = 'block';
-    else info.style.display = 'none';
-}
-
-addBtn.addEventListener("click", function () {
-    if(input.value.trim() === '') alert("Tên công việc không được để trống!");
-    else{
-        let todoE = {
-            id: ++id,
-            title: input.value.trim(),
-            status: false,
-        };
-        todoList.push(todoE);
-        showItems();
-        input.value = "";
-        checkEmptyList();
-    }
-});
-
-
+let id = 2;
 
 const showItems = () => {
-    list.innerHTML = '';
+  list.innerHTML = "";
+  if(todoList.length === 0) {
+    const info = document.createElement("p");
+    info.textContent = "Danh sách công việc trống!";
+    list.appendChild(info);
+  }else {
     todoList.forEach((e) => {
-        const li = document.createElement('li');
-        
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.addEventListener('change', function(event) {
-            e.status = checkbox.checked;
-            let targetedButton = event.target;
-            let targetedLi = targetedButton.parentNode;
-            let targetedTitle = targetedLi.querySelector("span");
-            targetedTitle.classList.toggle('checked');
-        })
+            const li = document.createElement("li");
 
-        const span = document.createElement('span');
-        span.classList.add('title');
-        span.textContent = e.title;
-        const editButton = document.createElement('button');
-        editButton.classList.add('edit-button');
-        editButton.textContent = 'Edit';
-        editButton.addEventListener("click", function (event) {
-            let targetedButton = event.target;
-            let targetedLi = targetedButton.parentNode;
-            let targetedTitle = targetedLi.querySelector("span");
-            let currentTitle = targetedTitle.innerText;
-            let newTitle = window.prompt("Enter the new title:", currentTitle);
-            if (newTitle !== null) {
-                e.title = newTitle;
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = e.status;
+            checkbox.addEventListener("change", function () {
+                e.status = checkbox.checked;
                 showItems();
-            }
-        })
+            });
 
+            const span = document.createElement("span");
+            span.classList.add("title");
+            span.textContent = e.title;
+            if(e.status) span.classList.add('checked');
 
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete-button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.addEventListener("click", function() {
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            editButton.addEventListener("click", function () {
+            let newTitle = window.prompt("Nhập tiêu đề mới:", e.title);
+            if (newTitle.trim() === '') alert("Tên công việc không được để trống!");
+            else{
+                    e.title = newTitle;
+                    showItems();
+                }
+            });
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener("click", function () {
             if (window.confirm("Bạn có chắc chắn muốn xóa?")) {
                 let index = todoList.findIndex((item) => item.id === e.id);
                 todoList.splice(index, 1);
             }
             showItems();
-            checkEmptyList();
-        })
+            });
 
-
-
-        li.appendChild(checkbox);
-        li.appendChild(span);
-        li.appendChild(editButton);
-        li.appendChild(deleteButton);
-        list.appendChild(li);
-    });
+            li.appendChild(checkbox);
+            li.appendChild(span);
+            li.appendChild(editButton);
+            li.appendChild(deleteButton);
+            list.appendChild(li);
+        });
+    }
 };
+
+addBtn.addEventListener("click", function () {
+    if (input.value.trim() === '') {
+      alert("Tên công việc không được để trống!");
+      input.value = "";
+    }
+    else {
+      let todoE = {
+        id: ++id,
+        title: input.value.trim(),
+        status: false,
+      };
+      todoList.push(todoE);
+      input.value = "";
+      showItems();
+    }
+  });
+
+showItems();
